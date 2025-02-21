72cce5773276fdec25f303630528073a32c14ce9
RIKIOPVIP1 <xbgmitest@gmail.com>
2025-02-22 00:43:11 +0530

    Update soul.c

--- soul.c
+++ soul.c
@@ -2,11 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/wait.h>
+#include <pthread.h>
 #include <time.h>
 
 void usage() {
